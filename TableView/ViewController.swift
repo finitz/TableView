4e2e526d8da3e0014798984447d7a947a9c6ee5b
NarekSafaryan <nareksafaryandev@gmail.com>
2019-10-24 17:05:00 +0400

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Narek Safaryan on 10/24/19.
+//  Copyright © 2019 Narek Safaryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
